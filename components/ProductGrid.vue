1fd94724b63929d760b2212387f468543e9a518a
Ruben Hernandez <hernandzruben9@gmail.com>
2024-11-18 02:06:28 -0500

    uhuh

--- components/ProductGrid.vue
+++ components/ProductGrid.vue
@@ -23,7 +23,7 @@
     >
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div
-            class="grid grid-cols-2
+            class="grid grid-cols-1
              sm:grid-cols-1
              md:grid-cols-3
              lg:grid-cols-4
